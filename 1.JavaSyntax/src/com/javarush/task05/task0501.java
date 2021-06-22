b7335a1ee20358f828f6f226102b2faae3a2272c
romanOlontsev <roverviner@yandex.ru>
2021-06-22 19:38:09 +0300

    Added task0501.

---
+++ 1.JavaSyntax/src/com/javarush/task05/task0501.java
@@ -0,0 +1,8 @@
+package com.javarush.task05;
+
+public class task0501 {
+    public static void main(String[] args) {
+        int[] intArray = new int[10];
+        double[] doubleArray = new double[10];
+    }
+}
