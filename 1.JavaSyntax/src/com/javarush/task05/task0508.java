6711657fa48657dd90e49819e61594a95b9a6f30
romanOlontsev <roverviner@yandex.ru>
2021-06-23 14:53:36 +0300

    Added task0508.

---
+++ 1.JavaSyntax/src/com/javarush/task05/task0508.java
@@ -0,0 +1,43 @@
+package com.javarush.task05;
+
+import java.util.Scanner;
+
+/*
+Удаляем одинаковые строки
+
+В этой задаче тебе нужно:
+Считать 6 строк и заполнить ими массив strings.
+Удалить повторяющиеся строки из массива strings, заменив их на null (null должны быть не строками "null").
+Примеры.
+Массив после чтения строк:
+{"Hello", "Hello", "World", "Java", "Tasks", "World"}
+Массив после удаления повторяющихся строк:
+{null, null, null, "Java", "Tasks", null}
+*/
+
+public class task0508 {
+    public static String[] strings;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        strings = new String[6];
+        for (int i = 0; i < 6; i++) {
+            strings[i] = scanner.nextLine();
+        }
+
+        for (int i = 0; i < strings.length; i++) {
+            for (int j = i + 1; j < strings.length; j++) {
+                if (strings[i]==null)
+                    break;
+                if (strings[i].equals(strings[j])) {
+                    strings[i] = null;
+                    strings[j] = null;
+                }
+            }
+        }
+        for (int i = 0; i < strings.length; i++) {
+            System.out.print(strings[i] + ", ");
+        }
+    }
+}
+
