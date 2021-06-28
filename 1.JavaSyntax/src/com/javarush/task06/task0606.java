82ddafc5239122acdeb2819f2ee83aebaa9d0c6a
romanOlontsev <roverviner@yandex.ru>
2021-06-28 15:00:28 +0300

    Added task0606.

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0606.java
@@ -0,0 +1,51 @@
+package com.javarush.task06;
+/*
+Универсальный солдат
+*/
+
+public class task0606 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod() {
+
+    }
+
+    public static void universalMethod(int a) {
+
+    }
+
+    public static void universalMethod(int a, int b) {
+
+    }
+
+    public static void universalMethod(int a, double b) {
+
+    }
+
+    public static void universalMethod(double a, double b) {
+
+    }
+
+    public static void universalMethod(String a) {
+
+    }
+
+    public static void universalMethod(String a, char b) {
+
+    }
+
+    public static void universalMethod(String a, double b) {
+
+    }
+
+    public static void universalMethod(String a, int b) {
+
+    }
+
+    public static void universalMethod(char a) {
+
+    }
+}
+
