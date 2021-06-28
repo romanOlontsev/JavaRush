bff02451a47c3cd746705d8594d1b19378954d85
romanOlontsev <roverviner@yandex.ru>
2021-06-28 13:50:16 +0300

    Added task0602.

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0602.java
@@ -0,0 +1,11 @@
+package com.javarush.task06;
+/*
+Упорядоченная информация
+*/
+
+public class task0602 {
+    public static void main(String[] args) {
+        System.out.println("Я — поэт, зовусь я Цветик.");
+        System.out.println("От меня вам всем приветик.");
+    }
+}
