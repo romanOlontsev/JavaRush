a608b98a6d0e4a9fe30dc9c0104a8f5f128bf16d
romanOlontsev <roverviner@yandex.ru>
2021-06-28 14:49:15 +0300

    Added task0605.

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0605.java
@@ -0,0 +1,31 @@
+package com.javarush.task06;
+
+/*
+Правильный порядок
+*/
+
+public class task0605 {
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        printArray(array);
+        reverseArray(array);
+        printArray(array);
+    }
+
+    public static void reverseArray(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[(array.length - i) - 1];
+            array[(array.length - i) - 1] = temp;
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + ", ");
+        }
+        System.out.println();
+    }
+}
+
