55dd5aa7db18edc0cf75a3c9c4343a79a5fc2f33
romanOlontsev <roverviner@yandex.ru>
2021-06-28 15:37:45 +0300

    Added task0608.

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0608.java
@@ -0,0 +1,16 @@
+package com.javarush.task06;
+
+/*
+Кубический калькулятор
+*/
+public class task0608 {
+
+    public static long cube(long input) {
+        return input*input*input;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
