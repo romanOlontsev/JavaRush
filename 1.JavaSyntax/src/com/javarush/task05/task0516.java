d53d6cb17f7c509df5e4850951fdc1cbab452a57
romanOlontsev <roverviner@yandex.ru>
2021-06-28 10:58:15 +0300

    Added task0516.

--- 1.JavaSyntax/src/com/javarush/task05/task0516.java
+++ 1.JavaSyntax/src/com/javarush/task05/task0516.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 
 /*
 Заполняем массив
+
+Реализуй статический метод main(String[]), который заполняет массив array значениями valueStart и valueEnd.
+Если длина массива четная, его первую половину нужно заполнить значениями valueStart, а вторую — valueEnd.
+Если длина массива нечётная, то большую часть заполнить значениями valueStart, меньшую — valueEnd.
+Для заполнения массива используй метод Arrays.fill(int[], int, int, int).
+При тестировании значения полей класса Solution будут разными, учти это.
 */
 public class task0516 {
 
@@ -12,8 +18,10 @@ public class task0516 {
     public static int valueEnd = 13;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int variable = array.length % 2 == 0 ? array.length / 2 : array.length / 2 + 1;
+        Arrays.fill(array, 0, variable, valueStart);
+        Arrays.fill(array, variable, array.length, valueEnd);
+
         System.out.println(Arrays.toString(array));
     }
-
 }
