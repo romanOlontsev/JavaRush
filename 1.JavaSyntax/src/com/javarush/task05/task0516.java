eb79eb4e11b7ef19be969f2c585c78c15b3c228e
romanOlontsev <roverviner@yandex.ru>
2021-06-28 10:33:07 +0300

    Added task0516.

---
+++ 1.JavaSyntax/src/com/javarush/task05/task0516.java
@@ -0,0 +1,19 @@
+package com.javarush.task05;
+
+import java.util.Arrays;
+
+/*
+Заполняем массив
+*/
+public class task0516 {
+
+    public static int[] array = new int[20];
+    public static int valueStart = 10;
+    public static int valueEnd = 13;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.println(Arrays.toString(array));
+    }
+
+}
