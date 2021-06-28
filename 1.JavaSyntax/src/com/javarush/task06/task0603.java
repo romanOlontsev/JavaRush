d2817040695ad4f363be116d9c666b2e78c21aa8
romanOlontsev <roverviner@yandex.ru>
2021-06-28 14:10:00 +0300

    Added task0603.

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0603.java
@@ -0,0 +1,22 @@
+package com.javarush.task06;
+
+/*
+Любимое блюдо
+*/
+
+public class task0603 {
+    public static void main(String[] args) {
+        String firstName = "Ольга";
+        String lastName = "Киприяновна";
+        String favouriteDish = "Пельмени";
+        printPersonInfo(firstName, lastName, favouriteDish);
+    }
+
+    public static void printPersonInfo(String name, String surname, String meal) {
+        System.out.println("Краткое досье:");
+        System.out.println("Имя: " + name);
+        System.out.println("Фамилия: " + surname);
+        System.out.println("Любимое блюдо: " + meal);
+    }
+}
+
