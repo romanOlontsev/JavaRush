96f0659fb9224a937ea3bdae3b3790114475e898
romanOlontsev <roverviner@yandex.ru>
2021-06-28 14:14:04 +0300

    Added task0604.

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0604.java
@@ -0,0 +1,24 @@
+package com.javarush.task06;
+
+/*
+Ливерпульская четверка
+*/
+
+public class task0604 {
+    public static void main(String[] args) {
+        String navigator = "Джон";
+        String pilot = "Пол";
+        String secondPilot = "Джордж";
+        String flightEngineer = "Ринго";
+        createCrew(navigator, pilot, secondPilot, flightEngineer);
+    }
+
+    public static void createCrew(String navigator, String pilot, String secondPilot, String flightEngineer) {
+        System.out.println("Четвёрка, которой предстоит покорить космос:");
+        System.out.println("Штурман: " + navigator);
+        System.out.println("Пилот: " + pilot);
+        System.out.println("Второй пилот: " + secondPilot);
+        System.out.println("Бортинженер: " + flightEngineer);
+    }
+}
+
