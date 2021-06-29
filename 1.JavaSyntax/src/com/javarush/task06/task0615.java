5aeb2e97e716b0fde0f7f497b6a008833b5fa9bf
romanOlontsev <roverviner@yandex.ru>
2021-06-29 12:47:22 +0300

    Changed class Earth variables to global constants.

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0615.java
@@ -0,0 +1,15 @@
+package com.javarush.task06;
+
+/*
+Все что нужно знать о Земле
+*/
+
+public class task0615 {
+
+    public class Earth {
+        public static final String NAME = "Земля";
+        public static final double SQUARE = 510_100_000;
+        public static final long POPULATION = 7_594_000_000L;
+        public static final long EQUATOR_LENGTH = 40_075_696;
+    }
+}
