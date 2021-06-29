77da2b5eb1179a574b9ee2bd6bbe1fbb310c4df8
romanOlontsev <roverviner@yandex.ru>
2021-06-29 12:38:42 +0300

    Changed pi variable to constant (line 8).

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0614.java
@@ -0,0 +1,17 @@
+package com.javarush.task06;
+
+/*
+Вспоминаем геометрию
+*/
+
+public class task0614 {
+    public static final double pi = 3.14;
+    public static double radius = 2.5;
+    public static double diameter = 5;
+
+    public static void main(String[] args) {
+        System.out.println("Плошадь круга равна: " + pi * radius * radius);
+        System.out.println("Периметр круга равен: " + pi * diameter);
+    }
+}
+
