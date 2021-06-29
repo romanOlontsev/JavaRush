4a7378726f7f2229a9e3bb90faf840a4478aab90
romanOlontsev <roverviner@yandex.ru>
2021-06-29 13:02:12 +0300

    Applied the this keyword to salary and position variables.

---
+++ 1.JavaSyntax/src/com/javarush/task06/task0616.java
@@ -0,0 +1,21 @@
+package com.javarush.task06;
+
+/*
+Учет работников
+*/
+
+public class task0616 {
+
+    public String name = "Amigo";
+    public String position = "Java developer";
+    public int salary = 10_000;
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
+
