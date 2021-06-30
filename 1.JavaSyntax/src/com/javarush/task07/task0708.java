bee9324033f36f70c618068a74d2c132b8eb9ac4
romanOlontsev <roverviner@yandex.ru>
2021-06-30 14:56:15 +0300

    Added the body of the setValues() method.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0708.java
@@ -0,0 +1,22 @@
+package com.javarush.task07;
+
+/*
+Правильные типы
+ */
+public class task0708 {
+    public static byte a;
+    public static short b;
+    public static int c;
+    public static long d;
+
+    public static void main(String[] args) {
+        setValues(100);
+    }
+
+    public static void setValues(long value) {
+        a = (byte) value;
+        b = (short) value;
+        c = (int) value;
+        d = value;
+    }
+}
