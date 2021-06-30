b01c5ee26f5dc9c767daff44a09ec0ba9018ba0c
romanOlontsev <roverviner@yandex.ru>
2021-06-30 14:14:41 +0300

    Minimum values assigned to primitive data types.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0703.java
@@ -0,0 +1,14 @@
+package com.javarush.task07;
+/*
+Минимальное целое
+*/
+public class task0703 {
+    byte aByte = Byte.MIN_VALUE;
+    short aShort = Short.MIN_VALUE;
+    int anInt = Integer.MIN_VALUE;
+    long aLong = Long.MIN_VALUE;
+    char aChar = Character.MIN_VALUE;
+    float aFloat = Float.MIN_VALUE;
+    double aDouble = Double.MIN_VALUE;
+    boolean aBoolean = Boolean.TRUE;
+}
