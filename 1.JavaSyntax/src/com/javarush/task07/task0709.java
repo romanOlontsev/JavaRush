554f4e76e319a81b7b194d7934bd327816321045
romanOlontsev <roverviner@yandex.ru>
2021-06-30 15:10:24 +0300

    Types of casting operators are placed.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0709.java
@@ -0,0 +1,13 @@
+package com.javarush.task07;
+
+/*
+Очень правильные типы
+*/
+
+public class task0709 {
+    long a = 109 + 15;
+    int b = (int) a * 2;
+    short c = (short) (a / b);
+    byte d = (byte) (a + b - c);
+}
+
