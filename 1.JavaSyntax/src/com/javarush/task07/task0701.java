6c2b94ae370f2b82b35d50ddc657bcc37ea04579
romanOlontsev <roverviner@yandex.ru>
2021-06-30 14:01:04 +0300

    Declared one variable of each primitive type.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0701.java
@@ -0,0 +1,16 @@
+package com.javarush.task07;
+
+/*
+Все возможные типы
+*/
+
+public class task0701 {
+    byte aByte;
+    short aShort;
+    int anInt;
+    long aLong;
+    char aChar;
+    float aFloat;
+    double aDouble;
+    boolean aBoolean;
+}
