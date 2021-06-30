8cdd8d30ae1c78e794f00eaa30c57cc3a3796b15
romanOlontsev <roverviner@yandex.ru>
2021-06-30 15:32:55 +0300

    Declared: 10 objects of type String, 5 objects of type int [], 2 Scanner objects.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0712.java
@@ -0,0 +1,31 @@
+package com.javarush.task07;
+
+/*
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class task0712 {
+    public static void main(String[] args) {
+
+        String a1 = new String();
+        String a2 = new String();
+        String a3 = new String();
+        String a4 = new String();
+        String a5 = new String();
+        String a6 = new String();
+        String a7 = new String();
+        String a8 = new String();
+        String a9 = new String();
+        String a10 = new String();
+        int[] b1 = new int[10];
+        int[] b2 = new int[10];
+        int[] b3 = new int[10];
+        int[] b4 = new int[10];
+        int[] b5 = new int[10];
+        Scanner c1 = new Scanner("");
+        Scanner c2 = new Scanner("");
+    }
+}
+
