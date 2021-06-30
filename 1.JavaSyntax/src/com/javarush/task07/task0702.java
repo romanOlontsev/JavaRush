0726cf484cd330fc6ae5e3f756ac3cbb133dd922
romanOlontsev <roverviner@yandex.ru>
2021-06-30 14:11:13 +0300

    Maximum values assigned to primitive data types.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0702.java
@@ -0,0 +1,16 @@
+package com.javarush.task07;
+
+/*
+Максимальное целое
+*/
+
+public class task0702 {
+    byte aByte = Byte.MAX_VALUE;
+    short aShort = Short.MAX_VALUE;
+    int anInt = Integer.MAX_VALUE;
+    long aLong = Long.MAX_VALUE;
+    char aChar = Character.MAX_VALUE;
+    float aFloat = Float.MAX_VALUE;
+    double aDouble = Double.MAX_VALUE;
+    boolean aBoolean = Boolean.FALSE;
+}
