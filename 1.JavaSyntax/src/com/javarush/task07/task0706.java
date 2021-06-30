e7500b416007f486e63e944aa003d5d9e048d72b
romanOlontsev <roverviner@yandex.ru>
2021-06-30 14:39:48 +0300

    Function call result - NaN.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0706.java
@@ -0,0 +1,16 @@
+package com.javarush.task07;
+
+/*
+Странное деление
+*/
+
+public class task0706 {
+    public static void main(String[] args) {
+        div(0.0, 0.0);
+    }
+
+    public static void div(double a, double b) {
+        System.out.println(b / a);
+    }
+}
+
