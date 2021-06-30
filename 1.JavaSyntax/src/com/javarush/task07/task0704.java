25cf271902322a8fa2a56812831ead4f9fc5806d
romanOlontsev <roverviner@yandex.ru>
2021-06-30 14:23:06 +0300

    Variable values are converted to exponential form.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0704.java
@@ -0,0 +1,14 @@
+package com.javarush.task07;
+
+/*
+Экспонентная запись числа
+*/
+
+public class task0704 {
+    double earthDiameter = 1.2742E+4;
+    double lightSpeed = 2.99792458E+8;
+    double uraniumAtomicMass = 2.380289E+2;
+    double averageBeeWeight = 8.5E-3;
+    double javaDeveloperSalary = 1.0000E+4;
+}
+
