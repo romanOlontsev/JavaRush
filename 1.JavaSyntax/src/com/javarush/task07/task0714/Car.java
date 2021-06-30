1f25158ecd033a6517c61482922133c0ad9fbcaa
romanOlontsev <roverviner@yandex.ru>
2021-06-30 15:51:28 +0300

    Installed class inheritance, removed duplicate variables.

---
+++ 1.JavaSyntax/src/com/javarush/task07/task0714/Car.java
@@ -0,0 +1,10 @@
+package com.javarush.task07.task0714;
+
+/*
+Наследование переменных
+*/
+
+public class Car extends Vehicle {
+    int wheelCount;
+    double weight;
+}
---
+++ 1.JavaSyntax/src/com/javarush/task07/task0714/Vehicle.java
@@ -0,0 +1,9 @@
+package com.javarush.task07.task0714;
+
+/*
+Наследование переменных
+*/
+
+public class Vehicle {
+    double maxSpeed;
+}
