f63c3e1e5a727e6d7431f120adfd0d31c3bea216
romanOlontsev <roverviner@yandex.ru>
2021-07-05 16:39:17 +0300

    Added implementation of setFlag(int,int), resetFlag(int, int), checkFlag(int,int) methods using bitwise shifts.

---
+++ 1.JavaSyntax/src/com/javarush/task08/task0814.java
@@ -0,0 +1,21 @@
+package com.javarush.task08;
+
+/*
+Флаги
+*/
+
+public class task0814 {
+
+    public static int setFlag(int number, int flagPos) {
+        return number | (1 << flagPos);
+    }
+
+    public static int resetFlag(int number, int flagPos) {
+        return number & ~(1 << flagPos);
+    }
+
+    public static boolean checkFlag(int number, int flagPos) {
+        return (number & (1 << flagPos)) == (1 << flagPos);
+    }
+}
+
