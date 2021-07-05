960927ecb9fac40260dfe0b1f969af3f23425979
romanOlontsev <roverviner@yandex.ru>
2021-07-05 14:53:00 +0300

    Replaced short class names with full ones.

---
+++ 1.JavaSyntax/src/com/javarush/task08/task0806.java
@@ -0,0 +1,14 @@
+package com.javarush.task08;
+
+/*
+Импорты: Part 1
+*/
+
+public class task0806 {
+
+    public static void main(java.lang.String[] args) {
+        java.util.ArrayList<java.lang.String> arrayList = new java.util.ArrayList<>();
+        java.io.BufferedReader bufferedReader = new java.io.BufferedReader(new java.io.StringReader(""));
+        java.math.BigInteger bigInteger = new java.math.BigInteger("123");
+    }
+}
