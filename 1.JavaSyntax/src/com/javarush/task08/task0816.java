f8f73d12ff1e99a4ad4e4d409bf6da07332b8242
romanOlontsev <roverviner@yandex.ru>
2021-07-05 17:05:32 +0300

    Output value changed.

---
+++ 1.JavaSyntax/src/com/javarush/task08/task0816.java
@@ -0,0 +1,14 @@
+package com.javarush.task08;
+
+/*
+Приоритеты
+*/
+
+public class task0816 {
+
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println(number + number * (number + ++number));
+    }
+}
+
