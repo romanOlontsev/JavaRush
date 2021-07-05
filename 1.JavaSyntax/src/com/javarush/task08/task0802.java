80d2a4be0df1cd32cba354ab4a0fc8e37f291d5e
romanOlontsev <roverviner@yandex.ru>
2021-07-05 11:55:13 +0300

    sqrt(double), cbrt(double), pow(double) methods changed.

---
+++ 1.JavaSyntax/src/com/javarush/task08/task0802.java
@@ -0,0 +1,21 @@
+package com.javarush.task08;
+
+/*
+Утильный класс: часть 2
+*/
+
+public class task0802 {
+
+    public static double sqrt(double a) {
+        return Math.sqrt(a);
+    }
+
+    public static double cbrt(double a) {
+        return Math.cbrt(a);
+    }
+
+    public static double pow(int number, int power) {
+        return Math.pow(number, power);
+    }
+}
+
