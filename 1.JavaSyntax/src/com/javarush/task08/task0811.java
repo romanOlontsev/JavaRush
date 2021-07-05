6259760c5ad957ad3dbc7130a7cd00f5822d113b
romanOlontsev <roverviner@yandex.ru>
2021-07-05 15:49:56 +0300

    Two lines are commented out so that the result is 5.

---
+++ 1.JavaSyntax/src/com/javarush/task08/task0811.java
@@ -0,0 +1,19 @@
+package com.javarush.task08;
+
+/*
+Побитовые операции
+*/
+
+public class task0811 {
+
+    public static void main(String[] args) {
+        int x = 7;
+        //x = x & 25;
+        x = x & 5;
+        //x = x & 3;
+        x = x & 12;
+        x = x | 1;
+        System.out.println(x);
+    }
+
+}
