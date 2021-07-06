95e05ff3fbc11b1109565ea65053f5aa7f8c591c
romanOlontsev <roverviner@yandex.ru>
2021-07-06 10:45:44 +0300

    Fixed initialization of character literals.

---
+++ 1.JavaSyntax/src/com/javarush/task09/task0904.java
@@ -0,0 +1,20 @@
+package com.javarush.task09;
+
+/*
+Символьные литералы
+*/
+
+public class task0904 {
+    public static char first = '\u004A';
+    public static char second = 'a';
+    public static char third = '\u0076';
+    public static char fourth = 'a';
+
+    public static void main(String[] args) {
+        System.out.print(first);
+        System.out.print(second);
+        System.out.print(third);
+        System.out.print(fourth);
+    }
+}
+
