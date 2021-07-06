74600e92e19376293268317dad109abc3bfff6aa
romanOlontsev <roverviner@yandex.ru>
2021-07-06 10:30:35 +0300

    Variable data types changed.

---
+++ 1.JavaSyntax/src/com/javarush/task09/task0902.java
@@ -0,0 +1,16 @@
+package com.javarush.task09;
+
+/*
+Литералы с плавающей точкой
+*/
+
+public class task0902 {
+
+    public double a = 0.;
+    public double b = .0;
+    public double c = 100D;
+    public double d = 100.0;
+    public double e = 1.11E5;
+    public float f = 200F;
+    public float g = 0.F;
+}
