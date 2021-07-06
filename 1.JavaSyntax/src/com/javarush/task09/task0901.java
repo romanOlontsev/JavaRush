d413c46d7df29a5357d7373b3bb1ecc73b28b591
romanOlontsev <roverviner@yandex.ru>
2021-07-06 09:53:37 +0300

    Changed the character and types of literals.

---
+++ 1.JavaSyntax/src/com/javarush/task09/task0901.java
@@ -0,0 +1,13 @@
+package com.javarush.task09;
+
+/*
+Целочисленные литералы
+*/
+
+public class task0901 {
+    public byte b = -128;
+    public short s = -32768;
+    public int i = 1_234_567_890;
+    public long l = 2_345_678_900L;
+}
+
