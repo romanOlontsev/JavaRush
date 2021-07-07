c62ed00e65c6bef2e4670262706fcb32a8d3ce99
romanOlontsev <roverviner@yandex.ru>
2021-07-07 14:36:12 +0300

    The string is displayed according to the assignment.

---
+++ 1.JavaSyntax/src/com/javarush/task09/task0909.java
@@ -0,0 +1,12 @@
+package com.javarush.task09;
+
+/*
+Экранирование символов
+*/
+
+public class task0909 {
+    public static void main(String[] args) {
+        System.out.println("It's a Windows path: \"C:\\Program Files\\Java\\jdk-13.0.0\\bin\"\n" +
+                "It's a Java string: \\\"C:\\\\Program Files\\\\Java\\\\jdk-13.0.0\\\\bin\\\"");
+    }
+}
