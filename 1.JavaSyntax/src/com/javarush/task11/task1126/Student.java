803060f3c72e9ffe72bf5dd16f027c015461af71
romanOlontsev <roverviner@yandex.ru>
2021-07-15 13:43:01 +0300

    Added constructor, changed access modifiers.

---
+++ 1.JavaSyntax/src/com/javarush/task11/task1126/Student.java
@@ -0,0 +1,13 @@
+package com.javarush.task11.task1126;
+
+/*
+Студент
+*/
+
+public class Student {
+    public String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+}
