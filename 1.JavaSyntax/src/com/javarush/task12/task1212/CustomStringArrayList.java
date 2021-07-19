3cd0e0d1e0d3970c2c4b49f6dc1483630f89c429
romanOlontsev <roverviner@yandex.ru>
2021-07-19 15:09:01 +0300

    The CustomStringArrayList class implements the grow() method that copies an array into a new array with a new length.

---
+++ 1.JavaSyntax/src/com/javarush/task12/task1212/CustomStringArrayList.java
@@ -0,0 +1,33 @@
+package com.javarush.task12.task1212;
+
+/*
+Создаем свой список
+*/
+
+public class CustomStringArrayList {
+
+    private int size;
+    private int capacity;
+    private String[] elements;
+
+    public CustomStringArrayList() {
+        capacity = 10;
+        size = 0;
+        elements = new String[capacity];
+    }
+
+    public void add(String element) {
+        if (size == capacity) {
+            grow();
+        }
+        elements[size] = element;
+        size++;
+    }
+
+    private void grow() {
+        capacity = (int)(capacity*1.5);
+        String[] temp = new String[capacity];
+        System.arraycopy(elements,0,temp,0,elements.length);
+        elements = temp;
+    }
+}
