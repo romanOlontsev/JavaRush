111018e8e7922b202161254aebb1e95ee0d840dd
romanOlontsev <roverviner@yandex.ru>
2021-08-03 13:21:23 +0300

    Implemented default void start() and default void stop() in the Vehicle interface.

---
+++ 1.JavaSyntax/src/com/javarush/task17/task1716/Vehicle.java
@@ -0,0 +1,17 @@
+package com.javarush.task17.task1716;
+
+/*
+Дорожное движение
+*/
+
+public interface Vehicle {
+    default void start() {
+        System.out.println("Начинаю движение.");
+    }
+
+    default void stop() {
+        System.out.println("Останавливаюсь.");
+    }
+
+    void move();
+}
